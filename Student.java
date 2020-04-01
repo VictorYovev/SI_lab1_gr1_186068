75ee937f468db8860ddd285faba34c36e2f535d5
VictorYovev <viktor.jovev@students.finki.ukim.mk>
2020-04-02 01:25:41 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -8,11 +8,54 @@ class Student {
 
 	//TODO seters & getters
 
+	public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Integer> getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(List<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
+
 	public double getAverage() {
-		//TODO
+		double average = 0.0;
+		for(int i=0; i<labPoints.size(); i++){
+			average+=labPoints[i];
+		}
+		return average/ labPoints.size();
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		return labPoints.size() > 8;
 	}
 }
